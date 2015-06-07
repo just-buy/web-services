f829ddc290d56902904698bde6b3447742a2b31a
Aritra Saha <aritrasaha.90@gmail.com>
2015-06-07 20:22:36 +0530

    updated REST API

--- src/main/java/com/hackathon/ultimate/hackers/ws/resources/JustBuyResource.java
+++ src/main/java/com/hackathon/ultimate/hackers/ws/resources/JustBuyResource.java
@@ -34,14 +34,14 @@ public class JustBuyResource {
         identityManager = injector.getInstance(IdentityManager.class);
         /*
          * Injector injector = Guice.createInjector(new HibernateModule());
-		 * final JustBuyManager justBuyManager = injector
-		 * .getInstance(JustBuyManager.class); final IdentityManager
-		 * identityManager = injector .getInstance(IdentityManager.class);
-		 */
+         * final JustBuyManager justBuyManager = injector
+         * .getInstance(JustBuyManager.class); final IdentityManager
+         * identityManager = injector .getInstance(IdentityManager.class);
+         */
         try {
             // Initializing users info
-
-            User user = new User("Asif Ahammed", "+919491259503",
+            User user;
+            user = new User("Asif Ahammed", "+919491259503",
                     "Koramangala 3rd Block, Banaglore",
                     "asifahammed@flipkart.com",
                     PasswordHasher.createHash("asif123"));
@@ -56,6 +56,7 @@ public class JustBuyResource {
                     "aritra.saha@flipkart.com",
                     PasswordHasher.createHash("aritra123"));
             identityManager.createUser(user);
+
             // Initializing categories
             Category category = new Category("Electronics", null);
             int categoryId = justBuyManager.createCategory(category);
@@ -67,11 +68,17 @@ public class JustBuyResource {
             justBuyManager.createCategory(category);
             category = new Category("Others ..", null);
             justBuyManager.createCategory(category);
+
             // Initializing item
-            Item item = new Item("Samsung Galaxy",
+            Item item;
+            item = new Item("Samsung Galaxy",
                     "Brand new samsung galaxy available", 200, new Date(),
                     user, "~/ads/item_1.png", false, ItemType.SELL);
-            justBuyManager.addItemToCategory(item, categoryId);
+            justBuyManager.addItemToCategory(item, 1);
+            item = new Item("Nexus 5",
+                    "Nexus 5 better than MI 3", 2000, new Date(),
+                    user, "~/ads/item_2.png", false, ItemType.SELL);
+            justBuyManager.addItemToCategory(item, 2);
         } catch (Exception e) {
             // do nothing
         }
@@ -91,34 +98,16 @@ public class JustBuyResource {
     /**
      * Method is used to get item.
      *
-     * @param itemId
+     * @param id
      * @return Item
      * @throws Exception
      */
-    @Path("/item/{itemId}")
     @GET
+    @Path("/item/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Item getItem(@PathParam(value = "itemId") Integer itemId)
+    public Item getItem(@PathParam(value = "id") Integer id)
             throws Exception {
-        return justBuyManager.getItem(itemId);
-    }
-
-    /**
-     * Method is used to get category.
-     *
-     * @param categoryId
-     * @return Category
-     * @throws Exception
-     */
-    @Path("/category/{categoryId}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Category getCategory(
-            @PathParam(value = "categoryId") Integer categoryId)
-            throws Exception {
-        final Category category = justBuyManager.getCategory(categoryId);
-        log.info("Category is {}", category);
-        return category;
+        return justBuyManager.getItem(id);
     }
 
     /**
@@ -127,8 +116,8 @@ public class JustBuyResource {
      * @return List of categories
      * @throws Exception
      */
-    @Path("/categories")
     @GET
+    @Path("/category")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Category> getCategories() throws Exception {
         List<Category> categories = justBuyManager.getCategories();
@@ -137,34 +126,84 @@ public class JustBuyResource {
     }
 
     /**
+     * Method is used to get category.
+     *
+     * @param id
+     * @return Category
+     * @throws Exception
+     */
+    @GET
+    @Path("/category/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Category getCategory(
+            @PathParam(value = "id") Integer id)
+            throws Exception {
+        final Category category = justBuyManager.getCategory(id);
+        log.info("Category is {}", category);
+        return category;
+    }
+
+    /**
      * Method is used to get items in a category.
      *
-     * @param categoryId
+     * @param id
      * @return List of Items
      * @throws Exception
      */
-    @Path("/category/items/{categoryId}")
     @GET
+    @Path("/category/{id}/item")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Item> getItemsInCategory(
-            @PathParam(value = "categoryId") Integer categoryId)
+            @PathParam(value = "id") Integer id)
             throws Exception {
         List<Item> itemsInCategory = justBuyManager
-                .getItemsInCategory(categoryId);
-        log.info("categoryId {}", categoryId);
+                .getItemsInCategory(id);
+        log.info("Category ID {}", id);
         log.info("items size {}", itemsInCategory.size());
-        log.info("Items in category {} are {}", categoryId, itemsInCategory);
+        log.info("Items in category {} are {}", id, itemsInCategory);
         return itemsInCategory;
+    }
+
+    /**
+     * Method is used to add item to category.
+     *
+     * @param item
+     * @param categoryId
+     * @throws Exception
+     */
+    @POST
+    @Path("/category/{id}/item")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void register(Item item,
+                         @PathParam(value = "id") Integer categoryId)
+            throws Exception {
+        justBuyManager.addItemToCategory(item, categoryId);
+    }
+
+    /**
+     * Method is used to get user.
+     *
+     * @param id
+     * @throws Exception
+     */
+    @GET
+    @Path("/user/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser(@PathParam(value = "id") Integer id)
+            throws Exception {
+        return identityManager.getUser(id);
     }
 
     /**
      * Method is used to login.
      *
+     * @param email
+     * @param password
      * @return <code>true</code> if user authentication is valid.
      * <code>false</code>
      */
-    @Path("/login")
     @POST
+    @Path("/user/login")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
     public boolean authenticate(@FormParam("email") String email,
@@ -183,40 +222,14 @@ public class JustBuyResource {
     /**
      * Method is used to create user.
      *
+     * @param user
      * @throws Exception
      */
-    @Path("/register/user")
     @POST
+    @Path("/user/register")
     @Consumes(MediaType.APPLICATION_JSON)
     public void register(User user) throws Exception {
         user.setPassword(PasswordHasher.createHash(user.getPassword()));
         identityManager.createUser(user);
-    }
-
-    /**
-     * Method is used to add item to category.
-     *
-     * @throws Exception
-     */
-    @Path("/new/item/{categoryId}")
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void register(Item item,
-                         @PathParam(value = "categoryId") Integer categoryId)
-            throws Exception {
-        justBuyManager.addItemToCategory(item, categoryId);
-    }
-
-    /**
-     * Method is used to get user.
-     *
-     * @throws Exception
-     */
-    @Path("/user/{userId}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public User getUser(@PathParam(value = "userId") Integer userId)
-            throws Exception {
-        return identityManager.getUser(userId);
     }
 }
